936833ca52857819224df42589b704dee912341c
Richard Remer <rich.remer@gmail.com>
2019-02-28 23:43:11 -0800

    new document action

--- src/gtext-app.c
+++ src/gtext-app.c
@@ -11,6 +11,36 @@ struct _GTextApp {
 
 G_DEFINE_TYPE(GTextApp, gtext_app, GTK_TYPE_APPLICATION);
 
+static void gtext_app_class_init(GTextAppClass*);
+static void gtext_app_init(GTextApp*);
+static void gtext_app_startup(GApplication*);
+static void gtext_app_activate(GApplication*);
+static void gtext_app_open(GApplication*, GFile*[], gint, const gchar*);
+static void new_activated(GSimpleAction*, GVariant*, gpointer);
+
+static GActionEntry actions[] = {
+    {"new", new_activated, NULL, NULL, NULL}
+};
+
+static const gchar* new_accels[2] = {"<Ctrl>N", NULL};
+
+GTextApp* gtext_app_new(void) {
+    return g_object_new(GTEXT_APP_TYPE,
+                        "application-id", "us.remer.gtext",
+                        "flags", G_APPLICATION_HANDLES_OPEN,
+                        NULL);
+}
+
+static void new_activated(GSimpleAction* action, GVariant* param, gpointer app) {
+    gtext_app_activate(G_APPLICATION(app));
+}
+
+static void gtext_app_class_init(GTextAppClass* class) {
+    G_APPLICATION_CLASS(class)->startup = gtext_app_startup;
+    G_APPLICATION_CLASS(class)->activate = gtext_app_activate;
+    G_APPLICATION_CLASS(class)->open = gtext_app_open;
+}
+
 static void gtext_app_init(GTextApp* app) {
     // implements GApplication interface
 }
@@ -18,6 +48,13 @@ static void gtext_app_init(GTextApp* app) {
 static void gtext_app_startup(GApplication* app) {
     // call parent implementation
     G_APPLICATION_CLASS(gtext_app_parent_class)->startup(app);
+
+    g_action_map_add_action_entries(G_ACTION_MAP(app),
+        actions, G_N_ELEMENTS(actions), app);
+
+    gtk_application_set_accels_for_action(
+        GTK_APPLICATION(app),
+        "app.new", new_accels);
 
     // setup fallback icon for all app windows
     GList* app_icon = gtext_load_icon_list("accessories-text-editor");
@@ -35,17 +72,4 @@ static void gtext_app_open(GApplication* app, GFile* files[], gint nfiles, const
     for (int i = 0; i < nfiles; i++) {
         g_print("  %s\n", g_file_get_basename(files[i]));
     }
-}
-
-static void gtext_app_class_init(GTextAppClass* class) {
-    G_APPLICATION_CLASS(class)->startup = gtext_app_startup;
-    G_APPLICATION_CLASS(class)->activate = gtext_app_activate;
-    G_APPLICATION_CLASS(class)->open = gtext_app_open;
-}
-
-GTextApp* gtext_app_new(void) {
-    return g_object_new(GTEXT_APP_TYPE,
-                        "application-id", "us.remer.gtext",
-                        "flags", G_APPLICATION_HANDLES_OPEN,
-                        NULL);
 }
