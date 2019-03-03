c631508a2c7b502fe07df91bc04efa14f2837a9e
Richard Remer <rich.remer@gmail.com>
2019-03-03 14:06:41 -0800

    fix implicit decl

--- inc/gtext-doc-win.h
+++ inc/gtext-doc-win.h
@@ -10,6 +10,7 @@ G_DECLARE_FINAL_TYPE (GTextDocWin, gtext_doc_win, GTEXT, DOC_WIN, GtkApplication
 GTextDocWin* gtext_doc_win_new(GTextApp*);
 GTextDocWin* gtext_doc_win_new_with_file(GTextApp*, GFile*);
 GFile* gtext_doc_win_get_file(GTextDocWin*);
+void gtext_doc_win_reload(GTextDocWin*);
 void gtext_doc_win_save(GTextDocWin*);
 void gtext_doc_win_save_as(GTextDocWin*);
 void gtext_doc_win_set_file(GTextDocWin*, GFile*);
